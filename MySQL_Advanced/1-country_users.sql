1b52251e5d2f054fca06abde355e2b4e9f518c1b
Your Name <you@example.com>
2026-03-16 15:20:09 +0400

    done until 6

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,4 @@
 -- Create a table named `users` with the following columns:
-DROP TABLE users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,

d67dd05e830354f283c7dd2787d403f775245956
Your Name <you@example.com>
2026-03-22 14:30:24 +0400

    gitpusher

---
+++ redis_basic/exercise.py
@@ -0,0 +1,17 @@
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self) -> None:
+        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+    
+    def get(self, key: str, fn):
+        data = self._redis.get(key)
+        if 

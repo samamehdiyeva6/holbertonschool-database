c8dc2d14f6193b9c010342047872e14fa8be9922
Your Name <you@example.com>
2026-03-30 17:42:30 +0400

    task 2 done

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
+"""
+This module defines a Cache class that interacts with a Redis database to store and retrieve data.
+"""
 
 import redis
 import uuid
 from typing import Callable, Optional, Union
+from functools import wraps
+
+def count_calls(f) -> Callable:
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        key = f.__qualname__
+        self._redis.incr(key)
+        return f(self, *args, **kwargs)
+    return wrapper
+
 
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)

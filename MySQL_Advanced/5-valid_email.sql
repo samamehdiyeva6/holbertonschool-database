6bcb3868a38b79456642d1b5d9858b846af65756
Your Name <you@example.com>
2026-04-13 16:19:33 +0400

    gitpusher

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,12 @@
--- sdfsd
+--  a SQL script that creates a trigger that resets the
+-- attribute valid_email only when the email has been changed
 DELIMITER $$
-CREATE TRIGGER valid_email_resetter
+CREATE TRIGGER reset_valid_email_update
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email <> NEW.email THEN
-        SET NEW.valid_email = 0;
+	IF OLD.email <> NEW.email THEN
+	    SET NEW.valid_email = 0;
     END IF;
 END $$
 DELIMITER ;

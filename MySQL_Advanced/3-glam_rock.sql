85b06bdb09d5af610d9ee774a51d02e186cfd383
Your Name <you@example.com>
2026-04-13 15:26:51 +0400

    gitpusher

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- List all bands with Glam rock as their main style, ranked by longevity
-SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
-    FROM metal_bands
-    WHERE style LIKE '%Glam rock%'
-    ORDER BY lifespan DESC;
+--kjbkjgkgjh
+SELECT band_name,
+       (IFNULL(split, 2024)) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

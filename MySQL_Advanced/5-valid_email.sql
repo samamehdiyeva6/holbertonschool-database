9dd53e6ff4d564ee53e8fc4e24cfa1291a5dbe9e
Your Name <you@example.com>
2026-04-13 16:06:46 +0400

    gitpusher

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER valid_email_resetter
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF OLD.email <> NEW.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END $$
